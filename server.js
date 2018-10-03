0c34202c3f1e3a65da1544b32117913f39371895
Cody Taft <cody.taft@gmail.com>
2018-10-03 15:49:41 -0600

    Connect database to server for palette post

--- server.js
+++ server.js
@@ -6,13 +6,25 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+app.set('port', process.env.PORT || 3000);
+
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
-app.set('port', process.env.PORT || 3000);
+app.use(function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.locals.title = 'Palette Picker';
 
@@ -85,25 +97,33 @@ app.post('/api/v1/projects', (request, response) => {
     });
 });
 
-// app.post('/api/v1/palettes', (request, response) => {
-//   const project = request.body;
+app.post('/api/v1/palettes', (request, response) => {
+  const palette = request.body;
 
-//   for (let requiredParameter of ['project_name']) {
-//     if (!project[requiredParameter]) {
-//       return response.status(422).send({
-//         error: `Expected format: { project_name: <string>}. You're missing a "${requiredParameter}" property`
-//       });
-//     }
-//   }
-//   database('projects')
-//     .insert(project, 'id')
-//     .then(project => {
-//       response.status(201).json(project);
-//     })
-//     .catch(error => {
-//       response.status(500).json({ error });
-//     });
-// });
+  for (let requiredParameter of [
+    'palette_name',
+    'project_id',
+    'color1',
+    'color2',
+    'color3',
+    'color4',
+    'color5'
+  ]) {
+    if (!palette[requiredParameter]) {
+      return response.status(422).send({
+        error: `Expected format: { palette_name: <string>, project_id: <integer>, color1: <string>, color2: <string>, color3: <string>, color4: <string>, color5: <String>}. You're missing a "${requiredParameter}" property`
+      });
+    }
+  }
+  database('palettes')
+    .insert(palette, 'id')
+    .then(palette => {
+      response.status(201).json(palette);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} running on ${app.get('port')}`);
