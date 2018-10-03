02022c3141c05e1616a2560433615ff0ac852369
Cody Taft <cody.taft@gmail.com>
2018-10-03 15:41:33 -0600

    Set up knex in server and project post

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
-
 app.set('port', process.env.PORT || 3000);
 
 app.locals.title = 'Palette Picker';
@@ -63,8 +66,44 @@ app.get('/', (request, response) => {
 });
 
 app.post('/api/v1/projects', (request, response) => {
-  // const palette
+  const project = request.body;
+
+  for (let requiredParameter of ['project_name']) {
+    if (!project[requiredParameter]) {
+      return response.status(422).send({
+        error: `Expected format: { project_name: <string>}. You're missing a "${requiredParameter}" property`
+      });
+    }
+  }
+  database('projects')
+    .insert(project, 'id')
+    .then(project => {
+      response.status(201).json(project);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
+
+// app.post('/api/v1/palettes', (request, response) => {
+//   const project = request.body;
+
+//   for (let requiredParameter of ['project_name']) {
+//     if (!project[requiredParameter]) {
+//       return response.status(422).send({
+//         error: `Expected format: { project_name: <string>}. You're missing a "${requiredParameter}" property`
+//       });
+//     }
+//   }
+//   database('projects')
+//     .insert(project, 'id')
+//     .then(project => {
+//       response.status(201).json(project);
+//     })
+//     .catch(error => {
+//       response.status(500).json({ error });
+//     });
+// });
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} running on ${app.get('port')}`);
