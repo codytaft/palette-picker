6fd62e475821da98e6b5d5716a848100b738a93e
Cody Taft <cody.taft@gmail.com>
2018-10-04 11:38:47 -0600

    Add production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,38 +11,29 @@ module.exports = {
     seeds: {
       directory: './seeds'
     }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './migrations'
+    },
+    useNullAsDefault: true
   }
+  //   staging: {
+  //     client: 'postgresql',
+  //     connection: {
+  //       database: 'my_db',
+  //       user: 'username',
+  //       password: 'password'
+  //     },
+  //     pool: {
+  //       min: 2,
+  //       max: 10
+  //     },
+  //     migrations: {
+  //       tableName: 'knex_migrations'
+  //     }
+  //   },
 };
-
-//   staging: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user: 'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   },
-
-//   production: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user: 'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   }
-// };
