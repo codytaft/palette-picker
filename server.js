5d1353521590333857930feb313a188dd557fc9a
Cody Taft <cody.taft@gmail.com>
2018-10-07 17:32:47 -0600

    Fixed HTML bug

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
+// app.use(function(req, res, next) {
+//   res.setHeader('Content-Type', 'application/json');
 
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 
 app.locals.title = 'Palette Picker';
 
